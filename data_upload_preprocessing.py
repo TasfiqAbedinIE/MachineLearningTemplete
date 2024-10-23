ec0348e7e34c6ae31b225f76be3557f615e2677a
TasfiqAbedin <tasfiq.ipe.aust@gmail.com>
2024-10-23 22:45:27 +0600

    version-1.5.0

---
+++ data_upload_preprocessing.py
@@ -0,0 +1,27 @@
+import pandas as pd
+import streamlit as st
+from navigation import Side_Bar
+
+# class streamlit_page_config:
+#     st.set_page_config(
+#         page_title="SEWING HOURLY PRODUCTION - SQUARE",
+#         layout= "wide",
+#         initial_sidebar_state='auto',
+#
+#     )
+#     st.markdown('<style>' + open('./style.css').read() + '</style>', unsafe_allow_html=True)
+#
+#     hide_footer_style = '''<style>.reportview-container .main footer {visibility: hidden;} </style>'''
+#     st.markdown(hide_footer_style, unsafe_allow_html=True)
+#     hide_menu_style = '''<style> #MainMenu {visibility: hidden;} </style>'''
+#     st.markdown(hide_menu_style, unsafe_allow_html=True)
+#
+#
+# streamlit_page_config()
+# side_bar = Side_Bar()
+# side_bar.make_sidebar()
+
+# Section-1: Uploading the DATASET --------------->
+
+def data_upload(file_path):
+    pass
--- main.py
+++ main.py
@@ -1,17 +1,33 @@
 import streamlit as st
-from MachineLearningTemp import MachineLearningTemplete
+from navigation import Side_Bar
+from data_upload_preprocessing import data_upload
 
-st.set_page_config(
-    page_title="Machine Learning Templete",
-    layout="wide",
-    initial_sidebar_state="collapsed",
-)
+class streamlit_page_config:
+    st.set_page_config(
+        page_title="MACHINE LEARNING TEMPLATE",
+        layout= "wide",
+        initial_sidebar_state='auto',
+
+    )
+    st.markdown('<style>' + open('./style.css').read() + '</style>', unsafe_allow_html=True)
+
+    hide_footer_style = '''<style>.reportview-container .main footer {visibility: hidden;} </style>'''
+    st.markdown(hide_footer_style, unsafe_allow_html=True)
+    hide_menu_style = '''<style> #MainMenu {visibility: hidden;} </style>'''
+    st.markdown(hide_menu_style, unsafe_allow_html=True)
 
 
-main_window = MachineLearningTemplete()
+streamlit_page_config()
+side_bar = Side_Bar()
+side_bar.make_sidebar()
 
-data_file = main_window.file_uploader()
-main_window.display_dataframe(data_file)
+user_info = "This is a boiler plate Machine learning template and focused to apply machine learning methods to basic stage. I wanted to built it so that people with no coding skill can apply Machine Learning into their dataset with minimum effort."
 
-if main_window.display_complete:
-    main_window.working_with_missing_values()
+# Section-1: Uploading the DATASET --------------->
+st.markdown("<p class=section_title>DATA UPLOAD</p>", unsafe_allow_html=True)
+raw_data_file = st.file_uploader("File Upload", type=["xlsx", "csv"], key="raw_data_file")
+if raw_data_file:
+    pass
+else:
+    st.info(user_info)
+

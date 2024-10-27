a6f903ec951d6e04fe19cebf1441081f28e68354
TasfiqAbedin <tasfiq.ipe.aust@gmail.com>
2024-10-27 21:01:08 +0600

    version-1.5.0

--- pages/Supervised_learning.py
+++ pages/Supervised_learning.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import streamlit as st
 from navigation import Side_Bar
+from MachineLearningTemp import MachineLearningTemplete
 
 class streamlit_page_config:
     st.set_page_config(
@@ -9,7 +10,7 @@ class streamlit_page_config:
         initial_sidebar_state='auto',
 
     )
-    st.markdown('<style>' + open('../style.css').read() + '</style>', unsafe_allow_html=True)
+    st.markdown('<style>' + open('style.css').read() + '</style>', unsafe_allow_html=True)
 
     hide_footer_style = '''<style>.reportview-container .main footer {visibility: hidden;} </style>'''
     st.markdown(hide_footer_style, unsafe_allow_html=True)
@@ -20,4 +21,30 @@ class streamlit_page_config:
 streamlit_page_config()
 side_bar = Side_Bar()
 side_bar.make_sidebar()
+
+user_info = "This is a boiler plate Machine learning template and focused to apply machine learning methods to basic stage. I wanted to built it so that people with no coding skill can apply Machine Learning into their dataset with minimum effort."
+
+# Section-1: Uploading the DATASET --------------->
+st.markdown("<p class=section_title>DATA UPLOAD</p>", unsafe_allow_html=True)
+raw_data_file = st.file_uploader("File Upload", type=["xlsx", "csv"], key="raw_data_file")
+if raw_data_file:
+    MC_Template = MachineLearningTemplete(raw_data_file)
+    file_type = MC_Template.check_file_ext()
+
+    if file_type == "csv":
+        with st.expander("DATA FRAME"):
+            uploaded_dataframe = pd.read_csv(raw_data_file)
+            st.dataframe(uploaded_dataframe, hide_index=True, use_container_width=True)
+    elif file_type == "xlsx":
+        excel_sheetname = st.text_input("Enter Sheet Name", key="excel_sheetname")
+        if excel_sheetname:
+            with st.expander("DATA FRAME"):
+                uploaded_dataframe = pd.read_excel(raw_data_file, sheet_name=excel_sheetname)
+                st.dataframe(uploaded_dataframe, hide_index=True, use_container_width=True)
+    else:
+        st.error("File type is not supported.")
+else:
+    st.info(user_info)
+
+
 

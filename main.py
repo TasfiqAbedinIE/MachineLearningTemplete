e3282fd9ce8a757e8f565f7192dcabfae6e3f999
TasfiqAbedin <tasfiq.ipe.aust@gmail.com>
2024-10-27 21:07:26 +0600

    version-1.5.0

--- main.py
+++ main.py
@@ -21,13 +21,5 @@ streamlit_page_config()
 side_bar = Side_Bar()
 side_bar.make_sidebar()
 
-user_info = "This is a boiler plate Machine learning template and focused to apply machine learning methods to basic stage. I wanted to built it so that people with no coding skill can apply Machine Learning into their dataset with minimum effort."
-
-# Section-1: Uploading the DATASET --------------->
-st.markdown("<p class=section_title>DATA UPLOAD</p>", unsafe_allow_html=True)
-raw_data_file = st.file_uploader("File Upload", type=["xlsx", "csv"], key="raw_data_file")
-if raw_data_file:
-    pass
-else:
-    st.info(user_info)
+st.switch_page("pages/Supervised_learning.py")
 
